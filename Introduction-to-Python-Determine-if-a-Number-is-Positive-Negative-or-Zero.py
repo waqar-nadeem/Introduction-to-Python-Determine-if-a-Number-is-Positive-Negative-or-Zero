901011833658b45c68bee4aad32d590e9899c3e7
waqar-nadeem <waqarnadeem088@gmail.com>
2025-11-13 21:24:14 +0500

    Create Introduction-to-Python-Determine-if-a-Number-is-Positive-Negative-or-Zero.py

---
+++ Introduction-to-Python-Determine-if-a-Number-is-Positive-Negative-or-Zero.py
@@ -0,0 +1,9 @@
+number = int(input("Enter an integer: "))
+
+if number >= 0:
+    if number == 0:
+        print("The number is Zero.")
+    else:
+        print("The number is Positive.")
+else:
+    print("The number is Negative.")
